e2ce6bc388ae0943b0f1de2f94759327e47fd0b5
Leonardo Theodoro <simplespinheiro@gmail.com>
2023-01-16 21:44:47 -0300

    :sparkles: NewTransactionModal component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles'
 
 import logoDtMoneyImg from '../../assets/logo-dt-money.svg'
 import * as Dialog from '@radix-ui/react-dialog'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
@@ -13,13 +14,7 @@ export function Header() {
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova transação</NewTransactionButton>
           </Dialog.Trigger>
-          <Dialog.Portal>
-            <Dialog.Overlay />
-            <Dialog.Content>
-              <Dialog.Title>Nova transação</Dialog.Title>
-              <Dialog.Close />
-            </Dialog.Content>
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
