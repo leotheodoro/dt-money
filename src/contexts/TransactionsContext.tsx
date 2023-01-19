23b2061800355206fa5e972ea676ac4023b92d73
Leonardo Theodoro <simplespinheiro@gmail.com>
2023-01-19 20:25:41 -0300

    :construction: creating new transaction

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,9 +10,17 @@ interface Transaction {
   createdAt: string
 }
 
+interface CreateTransactionInput {
+  description: string
+  price: number
+  category: string
+  type: 'income' | 'outcome'
+}
+
 interface TransactionsContextType {
   transactions: Transaction[]
   fetchTransactions: (query?: string) => Promise<void>
+  createTransaction: (data: CreateTransactionInput) => Promise<void>
 }
 
 interface TransactionProviderProps {
@@ -30,10 +38,29 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
     const response = await api.get('transactions', {
       params: {
         q: query,
+        _sort: 'createdAt',
+        _order: 'desc',
       },
     })
 
     setTransactions(response.data)
+  }
+
+  async function createTransaction({
+    description,
+    price,
+    category,
+    type,
+  }: CreateTransactionInput) {
+    const response = await api.post('transactions', {
+      description,
+      price,
+      category,
+      type,
+      createdAt: new Date(),
+    })
+
+    setTransactions((oldState) => [response.data, ...oldState])
   }
 
   useEffect(() => {
@@ -41,7 +68,9 @@ export function TransactionsProvider({ children }: TransactionProviderProps) {
   }, [])
 
   return (
-    <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
+    <TransactionsContext.Provider
+      value={{ transactions, fetchTransactions, createTransaction }}
+    >
       {children}
     </TransactionsContext.Provider>
   )
